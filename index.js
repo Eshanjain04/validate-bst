6d1a318acbaf91df6768490605a20dd3ddd1aa4b
Eshan Jain <eshan.jain@finverv.com>
2022-12-13 12:16:53 +0530

    added main function

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
+const Node = require('./nodes');
+
 const test ={
-    add: function addNum(a,b){
-        return a+b;
-    }
+    checkBST: validateBST
 }
+
+let root = new Node(5);
+root.left = new Node(1);
+root.right = new Node(7);
+root.right.left = new Node(6);
+root.right.right = new Node(9);
+
+function validateBST(root){
+    var helper = (root,max,min)=>{
+        if(!root) return true;
+        if(root.val>min && root.val<max){
+            return helper(root.left,root.val,min) && helper(root.right,max,root.val);
+        }
+        return false;
+    }
+
+    return helper(root,Number.MAX_SAFE_INTEGER,Number.NEGATIVE_INFINITY);
+}
+
+console.log(validateBST(root));
 
 module.exports = test;
